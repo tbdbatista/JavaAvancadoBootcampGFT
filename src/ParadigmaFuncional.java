e2e5222330a4df86754f9ea58baaf8802d20fdbb
Thiago B. D. Batista <tbdbatista@gmail.com>
2021-07-06 17:23:19 -0300

    iniciando módulo Paradigma Funcional em Java

---
+++ src/ParadigmaFuncional.java
@@ -0,0 +1,13 @@
+public class ParadigmaFuncional {
+    public static void main(String[] args) {
+
+/*----------------------------------------------------------------------------------------------------------------------
+            Paradigma Funcional no Java
+            Funções e imutabilidade em Paradigma Funcional
+            Lambda no Java
+            Recursividade
+----------------------------------------------------------------------------------------------------------------------*/
+
+
+    }
+}
